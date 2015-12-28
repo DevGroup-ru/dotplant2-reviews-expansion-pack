73b8d5caa465bc15063ed195a50958f7573c831e
matveev <matveev@devgroup.ru>
2015-12-28 13:49:34 +0300

    fixed

--- src/components/ConfigurationModel.php
+++ src/components/ConfigurationModel.php
@@ -24,7 +24,7 @@ class ConfigurationModel extends BaseConfigurationModel
     {
         return [
             'modules' => [
-                'ReviewsReviewsExt' => [
+                'ReviewsExt' => [
                     'class' => 'DotPlant\ReviewsExt\Module',
                 ],
             ],
